df34d8584d7fa4db34bd71daa6828b5c87a8af32
willylikestokayak <wccoker@gmail.com>
2017-09-11 13:30:54 -0700

    working on game logic

---
+++ js/app.js
@@ -0,0 +1,21 @@
+var suits = ["Spades", "Hearts", "Diamonds", "Clubs"];
+var values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
+var deck = new Array();
+
+function createDeck() {
+	deck = new Array();
+    for (var i = 0 ; i < values.length; i++) {
+    for(var x = 0; x < suits.length; x++) {
+      	var weight = parseInt(values[i]);
+         	if (values[i] == "J" || values[i] == "Q" || values[i] == "K")
+               	weight = 10;
+          	if (values[i] == "A")
+                weight = 11;
+        var card = { Value: values[i], Suit: suits[x], Weight: weight };
+            deck.push(card);
+            }
+        }
+    };
+
+
+
