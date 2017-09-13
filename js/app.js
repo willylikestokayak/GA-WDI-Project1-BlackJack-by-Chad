18811fdfff934b64312f30b89457dd39e4067e39
willylikestokayak <wccoker@gmail.com>
2017-09-13 10:26:35 -0700

    working on game logic

--- js/app.js
+++ js/app.js
@@ -1,27 +1,32 @@
 var startPlay = $('#startGame');
+var deal = $('#deal');
 var deckId;
 var cardlist = $('#cardslist');
 var dealerCards = $('#dealerCards');
 var playerCards = $('#playerCards');
 
-
-$.get('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=6').done(function(data) {
-	// console.log(data.deck_id);
-	localStorage.deckId = data.deck_id;
-});
-// console.log(localStorage.deckId);
-
-$.get('https://deckofcardsapi.com/api/deck/' + localStorage.deckId + '/draw/?count=2').done(function(data) {
-	$.each(data.cards, function(i, card) {
-	dealerCards.append('<p>' + card.value + card.suit + '</p>');
+$(startPlay).click(function(){ 
+	console.log("I am a click event in the start play function");
+	$.get('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=6').done(function(data) {
+		// console.log(data.deck_id);
+		localStorage.deckId = data.deck_id;
 	});
 });
 
-$.get('https://deckofcardsapi.com/api/deck/' + localStorage.deckId + '/draw/?count=2').done(function(data) {
-	$.each(data.cards, function(i, card) {
-	playerCards.append('<p>' + card.value + card.suit + '</p>');
-	});
+$(document).ready(function(){ 
+	$(deal).click(function(){
+		// Get dealers hand
+		$.get('https://deckofcardsapi.com/api/deck/' + localStorage.deckId + '/draw/?count=2').done(function(data) {
+			$.each(data.cards, function(i, card) {
+				console.log("Suit=" + card.suit + ", Rank=" + card.value);
+				dealerCards.append("<img src='" + card.image + "' >");
+			});
+		});
+		// get players hand
+		$.get('https://deckofcardsapi.com/api/deck/' + localStorage.deckId + '/draw/?count=2').done(function(data) {
+			$.each(data.cards, function(i, card) {
+				playerCards.append('<p>' + card.value + card.suit + card.image + '</p>');
+			});
+		});
+	});	
 });
-
-
-			
