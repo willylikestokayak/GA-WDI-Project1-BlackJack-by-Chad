868e5f0600bc743affbc7a5a567fba8da5b7b9c7
willylikestokayak <wccoker@gmail.com>
2017-09-13 16:13:28 -0700

    still working on game logic

--- js/app.js
+++ js/app.js
@@ -1,9 +1,19 @@
 var startPlay = $('#startGame');
 var deal = $('#deal');
+var hit = $('#hit');
 var deckId;
 var cardlist = $('#cardslist');
 var dealerCards = $('#dealerCards');
 var playerCards = $('#playerCards');
+var dealerArray = [];
+var playerArray = [];
+var faceCardValues = {
+	"JACK": 	10,
+	"QUEEN": 	10,
+	"KING": 	10,
+	"ACE": 		11,
+	"ACE-LOW": 	1, 
+};
 
 $(startPlay).click(function(){ 
 	console.log("I am a click event in the start play function");
@@ -15,18 +25,35 @@ $(startPlay).click(function(){
 
 $(document).ready(function(){ 
 	$(deal).click(function(){
-		// Get dealers hand
+		 
+		/*add function to display static image of card back until player presses the "stand button", after stand button is 
+		pressed, fire function to display both dealer cards and logic: if dealer hand <= 16, dealer takes a card (loop until
+		conditional is met) then fire else statement dealer stands if dealer hand >= 17 */
 		$.get('https://deckofcardsapi.com/api/deck/' + localStorage.deckId + '/draw/?count=2').done(function(data) {
 			$.each(data.cards, function(i, card) {
-				console.log("Suit=" + card.suit + ", Rank=" + card.value);
+				//console.log("Suit=" + card.suit + ", Rank=" + card.value);
 				dealerCards.append("<img src='" + card.image + "' >");
+					dealerArray.push(parseInt(card.value));
+					console.log("where I'm trying to push cards to array ",dealerArray);
 			});
 		});
 		// get players hand
 		$.get('https://deckofcardsapi.com/api/deck/' + localStorage.deckId + '/draw/?count=2').done(function(data) {
 			$.each(data.cards, function(i, card) {
-				playerCards.append('<p>' + card.value + card.suit + card.image + '</p>');
+				//console.log("Suit=" + card.suit + ", Rank+" + card.value);
+				playerCards.append("<img src='" + card.image + "'>");
 			});
 		});
 	});	
+});
+
+$(document).ready(function(){
+	$(hit).click(function(){ 
+		$.get('https://deckofcardsapi.com/api/deck/' + localStorage.deckId + '/draw/?count=1').done(function(data) {
+			$.each(data.cards, function(i, card) {
+				console.log("hit button test");
+				playerCards.append("<img src='" + card.image + "'>");
+			});
+		});
+	});
 });
