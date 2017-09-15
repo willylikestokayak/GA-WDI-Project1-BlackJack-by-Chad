d47c0541dbd042915255faf9ec4665adad56542e
willylikestokayak <wccoker@gmail.com>
2017-09-15 00:05:29 -0700

    working on game logic

--- js/app.js
+++ js/app.js
@@ -14,15 +14,17 @@ var playerHandTotal = 0;
 var dealerHandTotal = 0;
 var cards;
 var shuffleIndicator = 0;
+//var aceValue = $('#aceValue').prop('disabled');
 
 $(startPlay).click(function(){ 
 	$.get('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=6').done(function(data) {
 		localStorage.deckId = data.deck_id;
 	});
+	//aceValue.prop('disabled') = false;
 });
  
 $(deal).click(function(){
-	$.get('https://deckofcardsapi.com/api/deck/' + localStorage.deckId + '/draw/?count=52').done(function(data) {
+	$.get('https://deckofcardsapi.com/api/deck/' + localStorage.deckId + '/draw/?count=10').done(function(data) {
 		cards = data.cards;
 		for(let i = 0; i<4; i++){
 			if(i % 2 === 0){
@@ -60,24 +62,30 @@ $(deal).click(function(){
 				dealerCountOutput.val(dealerHandTotal);
 		});
 	});
+	// console.log("Who am I? playerArray " + playerArray + " or am I playerHandTotal? " + playerHandTotal);
+	// console.log(playerArray);  -- playerArray empty here
 
+// $(aceValue).click(function(){
+// 	console.log("is the aceValue button enabled?")
+// });
 $(hit).click(function(){ 
-	//cards = data.cards;
-		for(let i = 0; i<1; i++){
-			playerCards.append("<img src='" + cards[i].image + "'>");
-				if(cards[i].value === "KING" || cards[i].value === "QUEEN" || cards[i].value === "JACK") {
-					cards[i].value = 10;
-					playerArray.push(cards[i].value);
-					console.log("I am in the FACE CARD array, " + playerArray);
-				} else if (cards[i].value === "ACE") {
-					cards[i].value = 11;
-					playerArray.push(cards[i].value);
-					console.log("I am in the ACE array, " + playerArray);
-				} else {
-					playerArray.push(parseInt(cards[i].value));
-					console.log("I am in the NUMBERED CARD array, " + playerArray);
-				}
-		};
+	//need to call a card from cards variable somehow
+	//for(var i = 0; i < 2; i++){
+		playerCards.append("<img src='" + cards[i].image + "'>");
+			if(cards[i].value === "KING" || cards[i].value === "QUEEN" || cards[i].value === "JACK") {
+				cards[i].value = 10;
+				playerArray.push(cards[i].value);
+				console.log("Who am I? playerArray " + playerArray + " or am I playerHandTotal? " + playerHandTotal);
+				console.log(playerArray);
+			} else if (cards[i].value === "ACE") {
+				cards[i].value = 11;
+				playerArray.push(cards[i].value);
+				//console.log("I am in the ACE array, " + playerArray);
+			} else {
+				playerArray.push(parseInt(cards[i].value));
+				//console.log("I am in the NUMBERED CARD array, " + playerArray);
+			}
+	};
 		//not sure this for each loop is needed
 		// $.each(playerArray, function(n, value){
 		// 	playerHandTotal += playerArray[n] + playerHandTotal;
